1e740ab582f386f6d4e4cbda2b5aecced5dfb113
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-28 20:57:11 +0300

    Update menu02.cmd

--- menu02.cmd
+++ menu02.cmd
@@ -9,29 +9,29 @@ echo --- menu_02                             --- Other Program Installer ---    
 echo ------------------------------------------------------------------------------------------------------------------------
 REM Choose a program to install:
 echo [1]  'winget' TechPowerUp GPU-Z                                       
-echo [2]  (winget) Crystal Disk Info                                       
-echo [3]  (winget) Crystal Disk Mark                                      
-echo [4]  (winget) Core Temp                                                
-echo [5]  (winget) Lightshot                                                         
-echo [6]  (winget) Winget UI                                                
-echo [7]  (winget) Java 8                                                
-echo [8]  (winget) Free Download Manager                                                 
+echo [2]  'winget' Crystal Disk Info                                       
+echo [3]  'winget' Crystal Disk Mark                                      
+echo [4]  'winget' Core Temp                                                
+echo [5]  'winget' Lightshot                                                         
+echo [6]  'winget' Winget UI                                                
+echo [7]  'winget' Java 8                                                
+echo [8]  'winget' Free Download Manager                                                 
 echo [9]  "choco"  qBittorrent                                                    
 echo [10] "choco"  UniFi Network Application                                                  
-echo [11] (winget) Termius                                             
+echo [11] 'winget' Termius                                             
 echo [12] "choco"  FileZilla Server                             
-echo [13] (winget) inZip                                                   
-echo [14] (winget) Notion                                           
-echo [15] (winget) FL Studio                           
-echo [16] (winget) NDI 6 Tools                                              
-echo [17] (winget) NDI 6 Runtime                                 
-echo [18] (winget) Xtreme Download Manager
+echo [13] 'winget' inZip                                                   
+echo [14] 'winget' Notion                                           
+echo [15] 'winget' FL Studio                           
+echo [16] 'winget' NDI 6 Tools                                              
+echo [17] 'winget' NDI 6 Runtime                                 
+echo [18] 'winget' Xtreme Download Manager
 echo [19] {msstore} MSI Center 
 echo [20] {msstore} MSI Driver App Center 
-echo [21] (winget) CPUID CPU-Z MSI
-echo [22] (winget) MSI Afterburner
-echo [23] (winget) ASUS ArmouryCrate
-echo [24] (winget) CPUID ROG CPU-Z
+echo [21] 'winget' CPUID CPU-Z MSI
+echo [22] 'winget' MSI Afterburner
+echo [23] 'winget' ASUS ArmouryCrate
+echo [24] 'winget' CPUID ROG CPU-Z
 echo ------------------------------------------------------------------------------------------------------------------------
 echo [0] Exit
 echo ------------------------------------------------------------------------------------------------------------------------
@@ -56,8 +56,33 @@ if "%choice%"=="15" winget install ImageLine.FLStudio -e
 if "%choice%"=="16" winget install NDI.NDITools -e
 if "%choice%"=="17" winget install NDI.NDIRuntime -e
 if "%choice%"=="18" winget install subhra74.XtremeDownloadManager -e
-if "%choice%"=="19" winget install --id 9NVMNJCR03XV --accept-package-agreements --accept-source-agreements --silent ::MSI Center 9NVMNJCR03XV Unknown msstore
-if "%choice%"=="20" winget install --id 9P9WDH947752 --accept-package-agreements --accept-source-agreements --silent ::MSI Driver App Center 9P9WDH947752 Unknown msstore
+
+::---------------------------------------------------------------------------------------------------------------------------
+if "%choice%"=="19" (
+winget list --id 9NVMNJCR03XV -e >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Uygulama zaten yüklü. Güncelleme yapılıyor...
+    winget upgrade --id 9NVMNJCR03XV -e --accept-package-agreements --accept-source-agreements --silent
+) else (
+    echo Uygulama yüklü değil. Yükleniyor...
+    winget install --id 9NVMNJCR03XV -e --accept-package-agreements --accept-source-agreements --silent
+)
+)
+::MSI Center msstore 9NVMNJCR03XV
+::---------------------------------------------------------------------------------------------------------------------------
+if "%choice%"=="20" (
+winget list --id 9P9WDH947752 -e >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Uygulama zaten yüklü. Güncelleme yapılıyor...
+    winget upgrade --id 9P9WDH947752 -e --accept-package-agreements --accept-source-agreements --silent
+) else (
+    echo Uygulama yüklü değil. Yükleniyor...
+    winget install --id 9P9WDH947752 -e --accept-package-agreements --accept-source-agreements --silent
+)
+)
+::MSI Driver App Center 9P9WDH947752 Unknown msstore 9P9WDH947752
+::---------------------------------------------------------------------------------------------------------------------------
+
 if "%choice%"=="21" winget install CPUID.CPU-Z.MSI -e
 if "%choice%"=="22" winget install Guru3D.Afterburner -e
 if "%choice%"=="23" winget install Asus.ArmouryCrate
