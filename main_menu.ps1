f06ac27cae710e4c1c49b9f2aba6adcf1fb5407a
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-28 17:53:38 +0300

    Create main_menu.ps1

---
+++ main_menu.ps1
@@ -0,0 +1,26 @@
+# Clear the screen
+Clear-Host
+
+function Show-Menu {
+    Clear-Host
+    Write-Host "------------------------------------------------------------------------------------------------------------------------"
+    Write-Host "--- Main Menu                  --- Welcome to Software Install ---                                 CREATED BY MRLSx7 ---"
+    Write-Host "------------------------------------------------------------------------------------------------------------------------"
+    Write-Host "[1]  *** Program Install ***                       [6]  Browser Install"
+    Write-Host "[2]  Program Install                               [7]  VPN + Remote Install"
+    Write-Host "[3]  Security Install                              [8]  Media Player Install"
+    Write-Host "[4]  Developer Tools Install                       [9]  Chat Messenger Install"
+    Write-Host "[5]  Driver Install                                [10] Games Launcher Install"
+    Write-Host "------------------------------------------------------------------------------------------------------------------------"
+    Write-Host "[11] Microsoft Program Install                     [80] *** Winget Install  ***"
+    Write-Host "[12] Microsoft .NET Install                        [81] *** Chocolat Install ***"
+    Write-Host "------------------------------------------------------------------------------------------------------------------------"
+    Write-Host "[21] * Windows Fixed *                             [90] ** Standart PC Install All in One **"
+    Write-Host "[22] * Microsoft Fixed *"
+    Write-Host "------------------------------------------------------------------------------------------------------------------------"
+    Write-Host "[31] Setup Program Install ISO + EXE               [98] Windows Utility ( winutil )"
+    Write-Host "[32] Setup Microsoft Office Install EXE            [99] Microsoft Activation Scripts ( MAS )"
+    Write-Host "------------------------------------------------------------------------------------------------------------------------"
+    Write-Host "[0] Exit"
+    Write-Host "------------------------------------------------------------------------------------------------------------------------"
+}
