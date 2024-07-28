e7cfc74caeba60887ac977be49d65eddf9a6e612
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-28 20:16:15 +0300

    Update menu02.cmd

--- menu02.cmd
+++ menu02.cmd
@@ -7,325 +7,63 @@ echo ---------------------------------------------------------------------------
 echo --- menu_02                             --- Other Program Installer ---                            CREATED BY MRLSx7 ---
 echo ------------------------------------------------------------------------------------------------------------------------
 REM Choose a program to install:
-echo [1]  TechPowerUp GPU-Z                                      [21] Termius
-echo [2]  Crystal Disk Info                                      [22] FileZilla Server
-echo [3]  Crystal Disk Mark                                      [23]
-echo [4]  Core Temp                                              [24] WinZip 
-echo [5]                                                         [25] Notion 
-echo [6]  Lightshot                                              [26] FL Studio 
-echo [7]  Winget UI                                              [27] NDI 6 Tools 
-echo [8]                                                         [28] NDI 6 Runtime 
-echo [9]                                                         [29]
-echo [10]                                                        [30] Xtreme Download Manager
-echo [11] Java 8                                                 [31]
-echo [12] Free Download Manager                                  [32] MSI Center 
-echo [13]                                                        [33] MSI Driver - App Center 
-echo [14] qBittorrent                                            [34] CPUID CPU-Z MSI 
-echo [15] UniFi Network Application                              [35] MSI Afterburner 
-echo [16]                                                        [36] ASUS ArmouryCrate 
-echo [17]                                                        [37] CPUID ROG CPU-Z 
-echo [18]                                                        [38]
-echo [19]                                                        [39]
-echo [20]                                                        [40]
+echo [1]  TechPowerUp GPU-Z                                       
+echo [2]  Crystal Disk Info                                       
+echo [3]  Crystal Disk Mark                                      
+echo [4]  Core Temp                                                
+echo [5]  Lightshot                                                         
+echo [6]  Winget UI                                                
+echo [7]  Java 8                                                
+echo [8]  Free Download Manager                                                 
+echo [9]  qBittorrent                                                    
+echo [10] UniFi Network Application                                                  
+echo [11] Termius                                             
+echo [12] FileZilla Server                             
+echo [13] WinZip                                                   
+echo [14] Notion                                           
+echo [15] FL Studio                           
+echo [16] NDI 6 Tools                                              
+echo [17] NDI 6 Runtime                                 
+echo [18] Xtreme Download Manager
+echo [19] MSI Center
+echo [20] MSI Driver - App Center
+echo [21] CPUID CPU-Z MSI
+echo [22] MSI Afterburner
+echo [23] ASUS ArmouryCrate
+echo [24] CPUID ROG CPU-Z
 echo ------------------------------------------------------------------------------------------------------------------------
 echo [0] Exit
 echo ------------------------------------------------------------------------------------------------------------------------
-::------------------------------------------------------------------------------------------------------------------------
+::---------------------------------------------------------------------------------------------------------------------------
 set /p choice="Enter your choice (0,1,2,3...): "
-::------------------------------------------------------------------------------------------------------------------------
-if "%choice%"=="1" goto install_programB1
-if "%choice%"=="2" goto install_programB2
-if "%choice%"=="3" goto install_programB3
-if "%choice%"=="4" goto install_programB4
-if "%choice%"=="5" goto install_programB5
-if "%choice%"=="6" goto install_programB6
-if "%choice%"=="7" goto install_programB7
-if "%choice%"=="8" goto install_programB8
-if "%choice%"=="9" goto install_programB9
-if "%choice%"=="10" goto install_programB10
-if "%choice%"=="11" goto install_programB11
-if "%choice%"=="12" goto install_programB12
-if "%choice%"=="13" goto install_programB13
-if "%choice%"=="14" goto install_programB14
-if "%choice%"=="15" goto install_programB15
-if "%choice%"=="16" goto install_programB16
-if "%choice%"=="17" goto install_programB17
-if "%choice%"=="18" goto install_programB18
-if "%choice%"=="19" goto install_programB19
-if "%choice%"=="20" goto install_programB20
-if "%choice%"=="21" goto install_programB21
-if "%choice%"=="22" goto install_programB22
-if "%choice%"=="23" goto install_programB23
-if "%choice%"=="24" goto install_programB24
-if "%choice%"=="25" goto install_programB25
-if "%choice%"=="26" goto install_programB26
-if "%choice%"=="27" goto install_programB27
-if "%choice%"=="28" goto install_programB28
-if "%choice%"=="29" goto install_programB29
-if "%choice%"=="30" goto install_programB30
-if "%choice%"=="31" goto install_programB31
-if "%choice%"=="32" goto install_programB32
-if "%choice%"=="33" goto install_programB33
-if "%choice%"=="34" goto install_programB34
-if "%choice%"=="35" goto install_programB35
-if "%choice%"=="36" goto install_programB36
-if "%choice%"=="37" goto install_programB37
-if "%choice%"=="38" goto install_programB38
-if "%choice%"=="39" goto install_programB39
-if "%choice%"=="40" goto install_programB40
-::------------------------------------------------------------------------------------------------------------------------
+::---------------------------------------------------------------------------------------------------------------------------
+if "%choice%"=="1" winget install TechPowerUp.GPU-Z -e
+if "%choice%"=="2" winget install CrystalDewWorld.CrystalDiskInfo -e
+if "%choice%"=="3" winget install CrystalDewWorld.CrystalDiskMark -e
+if "%choice%"=="4" winget install ALCPU.CoreTemp -e
+if "%choice%"=="5" winget install Skillbrains.Lightshot -e
+if "%choice%"=="6" winget install SomePythonThings.WingetUIStore -e
+if "%choice%"=="7" winget install Oracle.JavaRuntimeEnvironment -e
+if "%choice%"=="8" winget install SoftDeluxe.FreeDownloadManager -e
+if "%choice%"=="9" choco install qbittorrent -y
+if "%choice%"=="10" choco install ubiquiti-unifi-controller -y
+if "%choice%"=="11" winget install Termius.Termius -e
+if "%choice%"=="12" choco install filezilla.server -y
+if "%choice%"=="13" winget install Corel.WinZip -e
+if "%choice%"=="14" winget install Notion.Notion -e
+if "%choice%"=="15" winget install ImageLine.FLStudio -e
+if "%choice%"=="16" winget install NDI.NDITools -e
+if "%choice%"=="17" winget install NDI.NDIRuntime -e
+if "%choice%"=="18" winget install subhra74.XtremeDownloadManager -e
+if "%choice%"=="19" powershell -NoProfile -ExecutionPolicy Bypass -Command "winget install --id 9NVMNJCR03XV --accept-package-agreements --accept-source-agreements --silent" ::MSI Center 9NVMNJCR03XV Unknown msstore
+if "%choice%"=="20" powershell -NoProfile -ExecutionPolicy Bypass -Command "winget install --id 9P9WDH947752 --accept-package-agreements --accept-source-agreements --silent" ::MSI Driver App Center 9P9WDH947752 Unknown msstore
+if "%choice%"=="21" winget install CPUID.CPU-Z.MSI -e
+if "%choice%"=="22" winget install Guru3D.Afterburner -e
+if "%choice%"=="23" winget install Asus.ArmouryCrate
+if "%choice%"=="24" winget install CPUID.CPU-Z.ROG
+::---------------------------------------------------------------------------------------------------------------------------
 if "%choice%"=="0" exit
-::------------------------------------------------------------------------------------------------------------------------
-goto menu_02
-::------------------------------------------------------------------------------------------------------------------------
-:install_programB1
-cls
-winget install TechPowerUp.GPU-Z -e
-pause
-goto menu_02
-::------------------------------------------------------------------------------------------------------------------------
-:install_programB2
-cls
-winget install CrystalDewWorld.CrystalDiskInfo -e
+::---------------------------------------------------------------------------------------------------------------------------
 pause
 goto menu_02
 ::---------------------------------------------------------------------------------------------------------------------------
-:install_programB3
-cls
-winget install CrystalDewWorld.CrystalDiskMark -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB4
-cls
-winget install ALCPU.CoreTemp -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB5
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB6
-cls
-winget install Skillbrains.Lightshot -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB7
-cls
-winget install SomePythonThings.WingetUIStore -e
-pause
-goto menu_02
-:install_programB8
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB9
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB10
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB11
-cls
-winget install Oracle.JavaRuntimeEnvironment -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB12
-cls
-winget install SoftDeluxe.FreeDownloadManager -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB13
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB14
-cls
-choco install qbittorrent -y
-::winget install qBittorrent.qBittorrent -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB15
-cls
-choco install ubiquiti-unifi-controller -y
-::winget install UbiquitiInc.UnifiNetworkApplication -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB16
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB17
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB18
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB19
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB20
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB21
-cls
-winget install Termius.Termius -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB22
-cls
-choco install filezilla.server -y
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB23
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB24
-cls
-winget install Corel.WinZip -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB25
-cls
-winget install Notion.Notion -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB26
-cls
-winget install ImageLine.FLStudio -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB27
-cls
-winget install NDI.NDITools -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB28
-cls
-winget install NDI.NDIRuntime -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB29
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB30
-cls
-winget install subhra74.XtremeDownloadManager -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB31
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB32
-cls
-::MSI Center 9NVMNJCR03XV Unknown msstore
-powershell -NoProfile -ExecutionPolicy Bypass -Command "winget install --id 9NVMNJCR03XV --accept-package-agreements --accept-source-agreements --silent"
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB33
-cls
-::MSI Driver & App Center 9P9WDH947752 Unknown msstore
-powershell -NoProfile -ExecutionPolicy Bypass -Command "winget install --id 9P9WDH947752 --accept-package-agreements --accept-source-agreements --silent"
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB34
-cls
-winget install CPUID.CPU-Z.MSI -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB35
-cls
-winget install Guru3D.Afterburner -e
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB36
-cls
-winget install Asus.ArmouryCrate
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB37
-cls
-winget install CPUID.CPU-Z.ROG
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB38
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB39
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-:install_programB40
-cls
-
-pause
-goto menu_02
-::---------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
